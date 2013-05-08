e0120b39b63a7cca7a407a45f12ec00c8c813e17
hjanetzek <hannes.janetzek@gmail.com>
2013-05-08 14:51:25 +0200

    testing

--- src/org/oscim/renderer/sublayers/TextureRenderer.java
+++ src/org/oscim/renderer/sublayers/TextureRenderer.java
@@ -55,7 +55,8 @@ public final class TextureRenderer {
 	}
 
 	public static Layer draw(Layer layer, float scale, Matrices m) {
-		GLState.test(false, false);
+		//GLState.test(false, false);
+
 		GLState.blend(true);
 
 		GLState.useProgram(mTextureProgram);
